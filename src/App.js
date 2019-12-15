12c83adf16e4231f4bc14326a5910c8f2ccaae85
Nick Askam <asknickfilms@gmail.com>
2019-12-14 20:24:37 -0600

    removing auto generated app classes

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import './App.css';
 
 function App() {
   return (
-    <div classname="App">
+    <div>
       <Navbar dark color="primary">
         <div classname="container">
           <NavbarBrand href="/"> Ristorante Con Fusion</NavbarBrand>
