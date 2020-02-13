2aba3114923cf2e058eb7739566415d8a0719fdb
Nick Askam <asknickfilms@gmail.com>
2020-02-13 14:40:12 -0600

    adding assignment 2

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -4,6 +4,7 @@ import Header from './HeaderComponent';
 import Menu from './MenuComponent';
 import Footer from './FooterComponent';
 import Contact from './ContactComponent';
+import About from './AboutComponent';
 import DishDetail from './DishdetailComponent';
 import { DISHES } from '../shared/dishes';
 import { COMMENTS } from '../shared/comments';
@@ -48,6 +49,7 @@ class Main extends Component {
 			<Header />
 			<Switch>
 				<Route path="/home" component={HomePage} />
+				<Route exact path="/aboutus" component={() => <About leaders={this.state.leaders} />} />
 				<Route exact path="/menu" component={() => <Menu dishes={this.state.dishes} />} />
 				<Route path="/menu/:dishId" component={DishWithId} />
 				<Route exact path="/contactus" component={Contact} />
