159263fc38ecc8e3481e92755a454df7aca4b293
Nick Askam <asknickfilms@gmail.com>
2020-04-25 17:04:37 -0500

    fetch post comment

--- src/components/DishdetailComponent.js
+++ src/components/DishdetailComponent.js
@@ -28,7 +28,7 @@ class CommentForm extends Component {
 
 	handleSubmit(values) {
 		this.toggleModal()
-		this.props.addComment(this.props.dishId, values.rating, values.author, values.comment);
+		this.props.postComment(this.props.dishId, values.rating, values.author, values.comment);
 	}
 	
 	render() {
@@ -121,7 +121,7 @@ class CommentForm extends Component {
 		);
 	}
 
-    function RenderComments({comments, addComment, dishId}) {
+    function RenderComments({comments, postComment, dishId}) {
 		return (
 			<div className="col-12 col-md-5 m-1">
 				<h4>Comments:</h4>
@@ -136,7 +136,7 @@ class CommentForm extends Component {
 					})}
 				</ul>
 				}
-				<CommentForm dishId={dishId} addComment={addComment} />
+				<CommentForm dishId={dishId} postComment={postComment} />
 			</div>
 		);
 	}
@@ -176,7 +176,7 @@ class CommentForm extends Component {
 					<div className="row">
 						<RenderDish dish={props.dish} />
 						<RenderComments comments={props.comments}
-							addComment={props.addComment}
+							postComment={props.postComment}
 							dishId={props.dish.id} />
 					</div>
 				</div>
