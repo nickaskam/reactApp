0bb727c09bcf0cfd74433953b0cb8fa1dc6e357a
Nick Askam <asknickfilms@gmail.com>
2020-04-26 14:02:10 -0500

    react animation components

--- src/components/DishdetailComponent.js
+++ src/components/DishdetailComponent.js
@@ -5,6 +5,7 @@ import { Link } from 'react-router-dom';
 import { Control, LocalForm, Errors } from 'react-redux-form';
 import { Loading } from './LoadingComponent';
 import { baseUrl } from '../shared/baseUrl';
+import { FadeTransform, Fade, Stagger } from 'react-animation-components';
 
 const required = (val) => val && val.length;
 const maxLength = (len) => (val) => !(val) || (val.length <= len);
@@ -110,13 +111,18 @@ class CommentForm extends Component {
 	function RenderDish({dish}) {
 		return(
 			<div className="col-12 col-md-5 m-1">
-				<Card>
-					<CardImg width="100%" src={baseUrl + dish.image} alt={dish.name} />
-					<CardBody>
-						<CardTitle>{dish.name}</CardTitle>
-						<CardText>{dish.description}</CardText>
-					</CardBody>
-				</Card>
+				<FadeTransform in 
+					transformProps={{
+						exitTransform: 'scale(0.5) translateY(-50%)'
+                }}>
+					<Card>
+						<CardImg width="100%" src={baseUrl + dish.image} alt={dish.name} />
+						<CardBody>
+							<CardTitle>{dish.name}</CardTitle>
+							<CardText>{dish.description}</CardText>
+						</CardBody>
+					</Card>
+				</FadeTransform>
 			</div>
 		);
 	}
@@ -126,14 +132,18 @@ class CommentForm extends Component {
 			<div className="col-12 col-md-5 m-1">
 				<h4>Comments:</h4>
 			   {comments && <ul className="list-unstyled">
+				   <Stagger in>
 				   {comments.map(comment => {
 					   return (
+						   <Fade in>
 						   <li key={comment.id}>
 							   <p>{comment.comment}</p>
 							   <p>-- {comment.author} , {new Intl.DateTimeFormat('en-US', {year: 'numeric', month: 'short', day: '2-digit'}).format(new Date(Date.parse(comment.date)))}</p>
 						   </li>
+						   </Fade>
 					   );
 					})}
+					</Stagger>
 				</ul>
 				}
 				<CommentForm dishId={dishId} postComment={postComment} />
